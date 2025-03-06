524169bd311356cca4b8c69611f0c0bb9aafc830
Aditya17-ai <130999483+Aditya17-ai@users.noreply.github.com>
2025-03-06 17:46:17 +0530

    Update twosum.java

--- src/twosum.java
+++ src/twosum.java
@@ -1,22 +1,22 @@
 public class twosum {
     public static void main(String[] args) {
         int arr[] = {10, 20, 30, 40, 50};
-        int target = 50;
-        int found = 0;
+        int t = 50;
+        int f = 0;
         for (int i = 0; i <= arr.length - 1; i++) {
             for (int j = i + 1; j <= arr.length - 1; j++) {
-                if (arr[i] + arr[j] == target) {
-                    System.out.println("Success: " + arr[i] + " + " + arr[j] + " = " + target);
-                    found = 1;
+                if (arr[i] + arr[j] == t) {
+                    System.out.println(t);
+                    f = 1;
                     break;
                 }
             }
-            if (found == 1) {
+            if (f == 1) {
                 break;
             }
         }
-        if (found == 0) {
-            System.out.println("No pairs found that sum to " + target);
+        if (f == 0) {
+            System.out.println("No pairs found that sum to " + t);
         }
     }
 }
